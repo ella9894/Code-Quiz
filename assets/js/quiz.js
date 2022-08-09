4d210235cd280e2d84669e941dd674e12a23c315
Gabriella_Akpo <ella9894@gmail.com>
2022-08-09 16:25:08 -0400

    quiz logic

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,15 +1,3 @@
-//GIVEN I am taking a code quiz
-//WHEN I click the start button
-//THEN a timer starts and I am presented with a question
-//WHEN I answer a question
-//THEN I am presented with another question
-//WHEN I answer a question incorrectly
-//THEN time is subtracted from the clock
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-//WHEN the game is over
-//THEN I can save my initials and score
-//document.getElelemtnbyID("quiz-score");
 var questions = [
     {
         quest: "Which shows the proper way to call the taskEdit function?",
@@ -38,68 +26,138 @@ var questions = [
     }
 
 ];
-var Question = 0;
-var time=quest
+var currentQuestion = 0;
+var time = 60;
+var timer;
 
-var butnEl = document.querySelector("#start");
-var Questions = document.querySelector("#");
+var homeScreen = document.querySelector("#home");
+var questionsEl = document.querySelector("#question");
+var timeEl = document.querySelector("#time");
+var options = document.querySelector("#options");
+var submit = document.querySelector("#submit");
+var QuizStart = document.querySelector("#start");
+var initials = document.querySelector("#initials");
+var comments = document.querySelector("#comments");
 
-//Create variables to store quiz question
-var question1 = document.createElement("p");
-    question1.className = "quest";
-    question1.textContent = "Which option shows the correct way to call the taskEdit function?";
-question1.appendChild();
+function beginQuiz() {
+    homeScreen.setAttribute("class", "hidden");
+    questionsEl.removeAttribute("class");
 
-var question2 = document.createElement("p");
-question2.className = "quest";
-question2.textContent = "For var candies=[“skittles”, “snickers”, “twix”, “kitkat”];, what will console.log(candies[2]); display?";
-question2.appendChild();
+    timer = setInterval(clock, 1500);
 
-var question3 = document.createElement("p");
-question3.className = "quest";
-question3.textContent= "What does page-content refer to in the variable declaration var pageContentEl = document.querySelector('#page-content') ?";
-question3.appendChild();
+    timer.textContent = time;
 
-var question4 = document.createElement("p");
-question4.className = "quest";
-question4.textContent = "What happens if you don’t put a ‘break’ after each case in a switchcase?"
-question4.appendChild();
+    getQuestion();
 
-var question5 = document.createElement("p");
-question5.className = "quest";
-question5.textContent = "What does if (!movieNameInput || !movieTypeInput) mean?";
-question5.appendChild();
+}
 
+function getQuestion() {
+    var nowQuestion = questions[currentQuestion];
+    var questionTopic = document.getElementById("q-topic");
+    questionTopic.textContent = nowQuestion.quest;
 
+    options.innerHTML = "";
 
+    nowQuestion.choices.forEach(function (choice, c) {
+        var choicebtn = document.createElement("button");
+        choicebtn.setAttribute("class", "options");
+        choicebtn.setAttribute("value", choice);
 
+        choicebtn.textContent = c + 1 + choice;
 
-var createQuestion = function () {
-    
-};
-b
+        choicebtn.onclick = AnswerValid;
 
-//Use mouse-click events to start the quiz
-butnEl.addEventListener("click", createQuestion);
-//Write for loops to cycle through the quiz
-//Write conditional statements to determine wrong and right answers
-for (var i = 0; i < Questions.length; i++) { 
-    var response = window.prompt(questions[i].prompt);
-    if (response == questions[i].answer) {
-        score + 10;
-        alert("Correct");
+        options.appendChild(choicebtn);
+    });
+}
+
+function AnswerValid() {
+    if (this.value== questions[currentQuestion].answer) {
+        comments.textContent = "Correct!";
+    } else {
+        
+        time -= 15;
+
+        if (time < 0) {
+            time = 0;
+        }
+        
+        timeEl.textContent = time;
+        comments.textContent = "Wrong!";
     }
-    else {
-        alert("WRONG!");
+
+    comments.setAttribute("class", "feedback");
+    setTimeout(function () {
+        comments.setAttribute("class", "comments hidden");
+    }, 1500);
+
+    currentQuestion++;
+
+    if (currentQuestion===questions.length) {
+        endQuiz();
+    } else { getQuestion(); }
+}
+
+function endQuiz() { 
+    clearInterval(timer);
+
+    var endGame = document.querySelector("#quiz-end");
+    endGame.removeAttribute("class");
+
+    var gameScore = document.querySelector("#score");
+    gameScore.textContent = time;
+
+    questionsEl.setAttribute("class", "hidden");
+
+}
+
+function clock () {
+    time--;
+    timer.textContent = time;
+
+    if (time <= 0) {
+        endQuiz();
     }
 }
 
+function saveScore() {
+    var init = initials.value.trim();
+    if (init !== "") {
+        var highscore = JSON.parse(window.localStorage.getItem("highscore")) || [];
+        var newScore = {
+            score: time,
+            init: init
+        };
+        highscore.push(newScore);
+        window.localStorage.setItem("highscore", JSON.stringify(highscore));
+        window.location.href = "highscore.html";
+    }
+}
 
-//Use key-press events to receive user input in the form of answers to quiz questions
+function verifyEnter(event) {
+    if (event.key === "Enter") {
+        saveScore();
+    }
+}
 
-//Create a time limit for the game using time functions
+submit.onclick = saveScore;
+
+QuizStart.onlick = beginQuiz;
+
+initials.onkeydown = verifyEnter;
 
 
 
-//Use client-side storage to store high scores
+
+
+
+
+
+
+
+
+
+
+
+
 
