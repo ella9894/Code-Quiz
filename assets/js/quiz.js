55b15adb4e612d5096fcbc0db44b1da1d9f1dae0
Gabriella_Akpo <ella9894@gmail.com>
2022-08-10 16:31:35 -0400

    moving highscore logic to quiz js file

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -142,10 +142,33 @@ function verifyEnter(event) {
 
 submit.onclick = saveScore;
 
-quizStart.onlick = beginQuiz;
+quizStart.addEventListener("click",beginQuiz);
 
 initials.onkeydown = verifyEnter;
 
+function displayScores() {
+    var highScores = JSON.parse(window.localStorage.getItem("highscore")) || [];
+    highScores.sort(function (x, y) {
+        return y.score - x.score;
+    });
+
+    highScores.forEach(function (score) {
+        var listEl = document.createElement("li");
+        listEl.textContent = score.initials + "-" + score.score;
+
+        var orderedList = document.getElementById("highscore");
+        orderedList.appendChild(listEl);
+    });
+}   
+
+function clearScores() {
+    window.localStorage.removeItem("highscore");
+    window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearScores;
+
+displayScores();
 
 
 
